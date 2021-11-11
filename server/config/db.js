07b734fd3c3a4b014394176796c34a01e7359211
Nertia1997 <nsh545@outlook.com>
2021-11-10 20:38:59 -0330

    Nicholas - Added the connection to MongoDB Atlas under server -> config -> db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,4 @@
-// database configuration file, just setting up empty for tracking - Roman
+//MongoDB Atlas Connection
+module.exports= {
+    "URI": "mongodb+srv://nicholas:NkvlOzSM3zipVopB@dbs.dyqpd.mongodb.net/tournaments_db?retryWrites=true&w=majority"
+}
