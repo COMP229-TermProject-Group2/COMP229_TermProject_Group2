cfe04a221b976c91b317921be5f08da4840717c4
Adriano Melquiades <apmelqui@gmail.com>
2021-11-20 09:35:05 -0400

    Authentication working

--- server/routes/tournaments.js
+++ server/routes/tournaments.js
@@ -3,25 +3,30 @@ const router = express.Router();
 
 const tournament_listController = require('../controllers/tournament_list');
 
-
+function requireAuth(req, res, next) {
+    if(!req.isAuthenticated()) {
+        return res.redirect('/login');
+    }
+    next();
+}
 
 //GET Route for the tournament_list page - READ Operation
 router.get('/', tournament_listController.displayTournamentList);
 
 //GET Route for displaying the Add page 
-router.get('/add', tournament_listController.displayAddPage);
+router.get('/add', requireAuth, tournament_listController.displayAddPage);
 
 //GET Route for processing the Add page - CREATE Operation
-router.post('/add', tournament_listController.processAddPage);
+router.post('/add', requireAuth, tournament_listController.processAddPage);
 
 //GET Route for displaying the Edit page - UPDATE Operation
-router.get('/edit/:id', tournament_listController.displayEditPage);
+router.get('/edit/:id', requireAuth, tournament_listController.displayEditPage);
 
 //POST Route for processing the Edit page - UPDATE Operation
-router.post('/edit/:id', tournament_listController.processEditPage); 
+router.post('/edit/:id', requireAuth, tournament_listController.processEditPage); 
 
 //GET Route to perform Deletion - DELETE Operation
-router.get('/delete/:id', tournament_listController.performDelete);
+router.get('/delete/:id', requireAuth, tournament_listController.performDelete);
 
 
 
